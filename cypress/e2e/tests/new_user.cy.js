ab557e7268e0f9d7aa65c8f03f8e1c9afcc1a10c
danielviorel <daniel.viorel@arcmedia.ch>
2023-02-07 09:10:54 +0100

    create Herzogproject

---
+++ cypress/e2e/tests/new_user.cy.js
@@ -0,0 +1,14 @@
+
+let config = Cypress.config()
+describe('Create new user.', function () {
+
+  beforeEach('before each test', function () {
+    cy.visit(`${config.baseUrl}`);
+    
+  });
+
+  it('create new user happyflow', function () {
+    
+  });
+
+});  
