2f1bc00d5710a2989e5320b65945b8c41187a434
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-02-15 16:20:18 +0100

    update yhe new version of magento

--- cypress/e2e/tests/checkout.cy.js
+++ cypress/e2e/tests/checkout.cy.js
@@ -4,7 +4,7 @@ describe('Checkout', function () {
 
   it('checkout', function () {
     cy.visit(`${config.baseUrl}/checkout/cart/`);
-    cy.get('.minicart-wrapper a[href="https://herzog-elmiger.ch/checkout/cart/"]')
+    cy.get('.minicart-wrapper a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/checkout/cart/"]')
       .click();
   });
 });
--- cypress/e2e/tests/navigation _and_categories.cy.js
+++ cypress/e2e/tests/navigation _and_categories.cy.js
@@ -3,63 +3,63 @@ describe('Navigation and categories', function () {
 
   it('check main navigation', function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/bodenbelage.html"] span').contains('Boden')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/bodenbelage.html"] span').contains('Boden')
       .should('be.visible') 
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/bodenbelage.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/brandschutzplatten.html"] span').contains('Brandschutz')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/brandschutzplatten.html"] span').contains('Brandschutz')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/brandschutzplatten.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/dammstoffe.html"] span').contains('Dämmen')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/dammstoffe.html"] span').contains('Dämmen')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/dammstoffe.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/holzwerkstoffe.html"] span').contains('Holzwerkstoffe')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/holzwerkstoffe.html"] span').contains('Holzwerkstoffe')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/holzwerkstoffe.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/kanten.html"] span').contains('Kanten')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/kanten.html"] span').contains('Kanten')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/kanten.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/kleben-dichten.html"] span').contains('Kleben')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/kleben-dichten.html"] span').contains('Kleben')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/kleben-dichten.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/massivholzplatten-bauholz.html"] span').contains('Massivholzpl./ Bauholz')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/massivholzplatten-bauholz.html"] span').contains('Massivholzpl./ Bauholz')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/massivholzplatten-bauholz.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/turen.html"] span').contains('Türen')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/turen.html"] span').contains('Türen')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
         expect(loc.pathname).to.equal('/turen.html');
     });
 
-    cy.get('.nav-desktop a[href="https://herzog-elmiger.ch/furnier-schnittholz.html"] span').contains('Furnier/Schnittholz')
+    cy.get('.nav-desktop a[href="https://p-2023-herzog-elmiger.appengine.flow.ch/furnier-schnittholz.html"] span').contains('Furnier/Schnittholz')
       .should('be.visible')
       .click();
     cy.location().should(loc => {
