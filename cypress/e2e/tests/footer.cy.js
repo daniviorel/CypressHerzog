32b13b4007d2917619259ed61994a5d8e0ec6565
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-02-22 16:03:56 +0100

    modified footer file

--- cypress/e2e/tests/footer.cy.js
+++ cypress/e2e/tests/footer.cy.js
@@ -6,7 +6,7 @@ describe('Footer', function () {
     cy.get('.link-list a[href="https://www.spa-aarau.ch/ueberuns/ueber-uns/team/"]').contains('Reklamationen')
       .should('be.visible')
       .click();
-});
+  });
   it('certificate', function () {
     cy.visit(`${config.baseUrl}`);
     cy.get('.link-list a[href="https://www.spa-aarau.ch/ueberuns/ueber-uns/zertifizierungen/"]').contains('Zertifizierungen')
