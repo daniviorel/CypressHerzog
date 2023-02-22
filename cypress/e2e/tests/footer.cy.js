13b39540424faaf813ad223cd2f4c5dcd1deade5
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-02-22 17:08:17 +0100

    footer done

--- cypress/e2e/tests/footer.cy.js
+++ cypress/e2e/tests/footer.cy.js
@@ -5,11 +5,34 @@ describe('Footer', function () {
     cy.visit(`${config.baseUrl}`);
     cy.get('.link-list a[href="https://www.spa-aarau.ch/ueberuns/ueber-uns/team/"]').contains('Reklamationen')
       .should('be.visible')
-      .click();
+      .click();  
+  it("webpage redirect", () => {
+    const page = {
+        "from": "https://test-spa-aarau-upgrade.herzog-elmiger.ch/",
+        "to": "https://www.spa-aarau.ch/ueberuns/ueber-uns/team/"
+    }
+
+    cy.visit(page.from, { failOnStatusCode: false });
+
+    cy.url()
+        .should("be.equals", page.to)
+    });
   });
+  
   it('certificate', function () {
     cy.visit(`${config.baseUrl}`);
     cy.get('.link-list a[href="https://www.spa-aarau.ch/ueberuns/ueber-uns/zertifizierungen/"]').contains('Zertifizierungen')
       .click();
+  it("webpage redirect", () => {
+    const page = {
+        "from": "https://test-spa-aarau-upgrade.herzog-elmiger.ch/",
+        "to": "https://www.spa-aarau.ch/ueberuns/ueber-uns/zertifizierungen/"
+    }
+
+    cy.visit(page.from, { failOnStatusCode: false });
+
+    cy.url()
+        .should("be.equals", page.to)
+    });
   });
 });
